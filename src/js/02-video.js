00452943147303e3e7726669202a6664a7699fd7
Serhii-Ter <serhij86@gmail.com>
2022-09-25 19:28:44 +0300

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeRef = document.querySelector('iframe');
+const player = new Player(iframeRef);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    console.log('Seconds played', data.seconds);
+    console.log('Percentage', data.percent);
+
+    console.log();
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
